f21c6e3fd15ed65b58e36fa779f5f126858cc77a
Nao_Suzuki <tky2302140@stu.o-hara.ac.jp>
2025-09-19 13:41:09 +0900

    [add] HWInspectorを追加

---
+++ HandlerWaltanForDxLib/src/HWInspector.cpp
@@ -0,0 +1,8 @@
+﻿#include "h/HWInspector.h"
+
+
+/**
+* @author   NZ
+* @date     25/09/19
+* @note		GUIインスペクターの実装ファイル
+*/
---
+++ HandlerWaltanForDxLib/src/h/HWInspector.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include "HWComponent.h"
+
+
+/**
+* @author   NZ
+* @date     25/09/19
+* @note		GUIインスペクターの定義
+*/
+
+
+/**
+ * @class	InspectorItem
+ * @brief	インスペクターに登録されるパラメータ群
+ */
+struct InspectorItem 
+{
+	std::string name;
+	void* data;
+	enum Type { INT, FLOAT } type;
+};
+
+
+/**
+ * @class	HWInspector
+ * @brief	GUIインスペクター
+ */
+class HWInspector
+{
+public:
+
+	/*     メンバ変数     */
+
+	//! 登録されているパラメータ
+	std::vector<InspectorItem> items;
+
+	
+	/*     メソッド     */
+
+	/**
+	 * @brief		インスペクターの描画
+	 * @author		NZ
+	 * @date		24/09/24
+	 */
+	void Draw();
+
+};
